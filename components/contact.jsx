12a49410b85b83168ecd408f0229667b6660b65c
Alivia Nytko <anytko@vols.utk.edu>
2024-04-21 14:04:38 -0400

    Added styles to contact form

--- components/contact.jsx
+++ components/contact.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom'; // Import NavLink for navigation
 import Navbar from './nav_bar';
+import './contact.css'
 
 const ContactForm = () => {
   const [form, setForm] = useState({
@@ -23,26 +24,37 @@ const ContactForm = () => {
     // Handle form submission here
   };
 
+
   return (
-    <>
-      <Navbar /> {/* Include the navbar at the top */}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input type="text" name="firstName" value={form.firstName} onChange={handleChange} placeholder="First Name" />
+    <div className="full-page">
+      <Navbar />
+      <h2 className='form-title'>Contact Us</h2>
+      <form className="full-form" onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label className='form-labels' htmlFor="firstName">First Name:</label>
+          <input className="input-box" type="text" name="firstName" value={form.firstName} onChange={handleChange} placeholder="First Name" />
         </div>
-        <div>
-          <input type="text" name="lastName" value={form.lastName} onChange={handleChange} placeholder="Last Name" />
+        <div className="input-group">
+          <label className='form-labels' htmlFor="lastName">Last Name:</label>
+          <input className="input-box" type="text" name="lastName" value={form.lastName} onChange={handleChange} placeholder="Last Name" />
         </div>
-        <div>
-          <input type="email" name="email" value={form.email} onChange={handleChange} placeholder="Email" />
+        <div className="input-group">
+          <label className='form-labels' htmlFor="email">Email:</label>
+          <input className="input-box" type="email" name="email" value={form.email} onChange={handleChange} placeholder="Email" />
         </div>
-        <div>
-          <textarea name="comment" value={form.comment} onChange={handleChange} placeholder="Comment" />
+        <div className="input-group">
+          <label className='form-labels' htmlFor="comment">Comment:</label>
+          <textarea rows="10"
+              cols="20" className="input-box" name="comment" value={form.comment} onChange={handleChange} placeholder="Comment" />
         </div>
-        <button type="submit">Send</button>
+        <div className="button-div">
+          <button type="submit">Send</button>
+        </div>
       </form>
-    </>
+    </div>
   );
 };
 
 export default ContactForm;
+
+
